6dc6cad30ff4237066064d2b5847159df5afe69c
cooo002 <cooo002@naver.com>
2019-07-01 14:48:19 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by 김재석 on 01/07/2019.
+//  Copyright © 2019 김재석. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
